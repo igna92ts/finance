2759605ff64e03770f99982fa8f233d596157d14
Ignacio Torres <igna_torres92@hotmail.com>
2018-08-07 18:34:44 -0300

    fixed handler

--- lambda/handler.js
+++ lambda/handler.js
@@ -3,9 +3,9 @@
 const tree = require('./tree');
 
 module.exports.createTree = (event, context, callback) => {
-  if (!event.body) callback(new Error('no body'));
+  if (!event.body) return callback(new Error('no body'));
   try {
-    const params = JSON.parse(event.body);
+    const params = event.body;
     if (!params.features || !params.data) return callback(new Error('no features or data'));
     const newTree = tree.buildTree(params.features, params.data);
     const response = { body: JSON.stringify({ tree: newTree }) };
